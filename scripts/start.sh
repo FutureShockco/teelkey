1146e22eb93cc463b946ebe3d7a288c27d9fdf67
hightouch67 <cetinkaya.ozden@gmail.com>
2019-11-07 20:22:23 +0000

    cgf

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Ports configuration
-#export HTTP_PORT=3001
-#export P2P_PORT=6001
+export HTTP_PORT=3001
+export P2P_PORT=6001
 
 # MongoDB configuration
-#export DB_NAME=avalon
-#export DB_URL=mongodb://localhost:27017
+export DB_NAME=teelkey
+export DB_URL=mongodb://localhost:27017
 
 # Peering configuration
 #export OFFLINE=1
@@ -22,11 +22,12 @@ export LOG_LEVEL=debug
 export REPLAY_OUTPUT=100
 
 # default peers to connect with on startup
-#export PEERS=ws://35.203.37.221:6001
+export PEERS=
+#ws://35.203.37.221:6001
 
 # your user and keys (only useful for active node owners)
-export NODE_OWNER=observer-node
-export NODE_OWNER_PUB=dTuBhkU6SUx9JEx1f4YEt34X9sC7QGso2dSrqE8eJyfz
+export NODE_OWNER=master
+export NODE_OWNER_PUB=21NuxpQXQQ2ujxepDekTKcsLed7m44yf5aKiA8VYHZAk7
 export NODE_OWNER_PRIV=34EpMEDFJwKbxaF7FhhLyEe3AhpM4dwHMLVfs4JyRto5
 
 node --stack-size=65500 src/main
--- src/config.js
+++ src/config.js
@@ -61,7 +61,7 @@ var config = {
             // if false master can create accounts with usernames without burning tokens
             masterPaysForUsernames: false,
             // the master account public original key
-            masterPub: 'j7upSXTgbDQZLEhRQZkXxzEnwyJvXctNvLCRBLkutUaD',
+            masterPub: '21NuxpQXQQ2ujxepDekTKcsLed7m44yf5aKiA8VYHZAk7',
             // the maximum time drift in ms before a transaction is invalid
             maxDrift: 200,
             // the max length of a transfer memo
