770063e1ad36a0dbf1b3fcc3a8d733979278e10a
lagreve <rapmatic@hotmail.fr>
2020-01-17 17:15:15 +0100

    add sign for unsigned transaction master key reset

--- src/transactions/changePassword.js
+++ src/transactions/changePassword.js
@@ -4,6 +4,11 @@ module.exports = {
         if (!validate.publicKey(tx.data.pub, config.accountMaxLength)) {
             cb(false, 'invalid tx data.pub'); return
         }
+        //TODO
+        if(tx.sender !== config.masterName)
+        {
+            cb(false, 'only master can reset keys'); return
+        }
         cb(true)
     },
     execute: (tx, ts, cb) => {
